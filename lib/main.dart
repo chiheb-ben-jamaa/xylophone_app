56ba2a8296f98d5c749de5382491169dc1eecb87
chiheb ben jamaa <chihabbenjamaa04@gmail.com>
2019-09-08 01:07:46 +0200

    First Commit Happy Coding !

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(Xylophone_class());
+
+class Xylophone_class extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(child:
+        Container())
+      ),
+    );
+  }
+
+}
